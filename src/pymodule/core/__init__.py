ecbdc1f5dd0f46ffb8e23429dc51d38694309f66
imc <ivan.cenov@rnd.bg>
2024-10-08 17:27:38 +0300

    RT #88: Added goodbye_from_core_module_b

--- src/pymodule/core/__init__.py
+++ src/pymodule/core/__init__.py
@@ -1,4 +1,4 @@
 # core/__init__.py
 
 from .core_module_a import hello_from_core_module_a, goodbye_from_core_module_a
-from .core_module_b import hello_from_core_module_b
+from .core_module_b import hello_from_core_module_b, goodbye_from_core_module_b
--- src/pymodule/core/core_module_b.py
+++ src/pymodule/core/core_module_b.py
@@ -3,3 +3,7 @@
 def hello_from_core_module_b() -> int:
     print(f"Hello from core_module_b")
     return 2
+
+def goodbye_from_core_module_b() -> int:
+    print(f"Goodbyte from core_module_b")
+    return -2
