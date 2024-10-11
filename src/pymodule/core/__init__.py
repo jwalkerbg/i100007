02dfc076a3843b960807229c26c0a4907de470e1
imc <i_cenov@botevgrad.com>
2024-10-11 16:27:14 +0300

    RT #92: Added tests for Config.deep_update()

--- src/pymodule/core/__init__.py
+++ src/pymodule/core/__init__.py
@@ -2,3 +2,4 @@
 
 from .core_module_a import hello_from_core_module_a, goodbye_from_core_module_a
 from .core_module_b import hello_from_core_module_b, goodbye_from_core_module_b
+from .config import Config
