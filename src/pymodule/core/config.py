376d03abe79b484a2c04b765267ef993e50a1f9c
imc <i_cenov@botevgrad.com>
2024-12-30 23:11:48 +0200

    Using import alias from tomllib and tomli.

--- src/pymodule/core/config.py
+++ src/pymodule/core/config.py
@@ -13,9 +13,9 @@ logger = getAppLogger(__name__)
 
 # Check Python version at runtime
 if sys.version_info >= (3, 11):
-    import tomllib  # Use the built-in tomllib for Python 3.11+
+    import tomllib as toml # Use the built-in tomllib for Python 3.11+
 else:
-    import tomli  # Use the external tomli for Python 3.7 to 3.10
+    import tomli as toml # Use the external tomli for Python 3.7 to 3.10
 
 class Config:
     def __init__(self) -> None:
@@ -90,15 +90,12 @@ class Config:
         try:
             # Open the file in binary mode (required by both tomli and tomllib)
             with open(file_path, 'rb') as f:
-                if sys.version_info >= (3, 11):
-                    return tomllib.load(f)  # Use tomllib for Python 3.11+
-                else:
-                    return tomli.load(f)  # Use tomli for Python 3.7 - 3.10
+                return toml.load(f)
 
         except FileNotFoundError as e:
             logger.error(f"{e}")
             raise e  # Optionally re-raise the exception if you want to propagate it
-        except (tomli.TOMLDecodeError if sys.version_info < (3, 11) else tomllib.TOMLDecodeError) as e:
+        except toml.TOMLDecodeError as e:
             logger.error(f"Error: Failed to parse TOML file '{file_path}'. Invalid TOML syntax.")
             raise e  # Re-raise the exception for further handling
         except Exception as e:
