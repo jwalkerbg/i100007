94613e1fe1f0e3eb7c80fd0376894c1c602cac24
imc <i_cenov@botevgrad.com>
2024-12-30 23:00:54 +0200

    Corrected typing error

--- src/pymodule/utils/utilities.py
+++ src/pymodule/utils/utilities.py
@@ -7,6 +7,6 @@ logger = getAppLogger(__name__)
 def hello_from_utils() ->  None:
     logger.info(f"Hello from utils")
 
-# this function is used to demostrate unittest and pytest styles of unit testing
+# this function is used to demonstrate unittest and pytest styles of unit testing
 def sumator(a:int, b:int, c:int) -> int:
     return a + b + c
