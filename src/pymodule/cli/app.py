284ef09e07d966c4d95b0eb7193f3f72c352c99c
imc <i_cenov@botevgrad.com>
2025-12-05 22:52:48 +0200

    RT #257: Added try block in main to catch uncaught exceptions.

--- src/pymodule/cli/app.py
+++ src/pymodule/cli/app.py
@@ -12,20 +12,23 @@ logger = get_app_logger(__name__)
 def main() -> None:
     """Main entry point of the CLI."""
 
-    # Step 1: Collect configuration from defaults, configuration file, and environment variables and CLI options
-    cfg = get_app_configuration()
-    # Step 2: Setup logging according to collected configuration
-    setup_logging(cfg.config['logging']['verbose'], cfg.config['logging']['log_prefix'], cfg.config['logging']['use_color'], cfg.config['logging']['use_string_handler'])
+    try:
+        # Step 1: Collect configuration from defaults, configuration file, and environment variables and CLI options
+        cfg = get_app_configuration()
+        # Step 2: Setup logging according to collected configuration
+        setup_logging(cfg.config['logging']['verbose'], cfg.config['logging']['log_prefix'], cfg.config['logging']['use_color'], cfg.config['logging']['use_string_handler'])
 
-    # Step 3: Show version info or run the application with collected configuration
-    if cfg.config['logging']['version_option']:
-        # Step 3a: Show version information
-        logger.info("Version information requested")
-        app_version = pkg_version("pymodule")
-        print(f"pymodule {app_version}")
-    else:
-        # Step 3b: Run the application with the collected configuration
-        run_app(cfg)
+        # Step 3: Show version info or run the application with collected configuration
+        if cfg.config['logging']['version_option']:
+            # Step 3a: Show version information
+            logger.info("Version information requested")
+            app_version = pkg_version("pymodule")
+            print(f"pymodule {app_version}")
+        else:
+            # Step 3b: Run the application with the collected configuration
+            run_app(cfg)
+    except Exception as e:
+        logger.error("The application could not be started because of errors. %s", str(e))
 
 if __name__ == "__main__":
     main()
